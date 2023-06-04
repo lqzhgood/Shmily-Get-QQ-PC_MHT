8ea8bd9d96b0cbd9671a6796a0c0fc708afd004b
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-06-04 23:10:58 +0800

    🦄 refactor(规整输出文件):

--- config.js
+++ config.js
@@ -2,18 +2,22 @@
  * @Description:
  * @Author: lqzh
  * @Date: 2020-02-28 15:47:38
- * @LastEditTime: 2023-01-31 16:04:58
+ * @LastEditTime: 2023-06-04 23:05:52
  */
-module.exports = {
+
+const fs = require("fs-extra");
+const path = require("path");
+
+const config = {
     direction: {
         name: {
-            go: ['昵称1', '昵称2', '昵称3'],
-            come: ['昵称1', '昵称2', '昵称3'],
+            go: ["昵称1", "昵称2", "昵称3"],
+            come: ["昵称1", "昵称2", "昵称3"],
         },
         // 根据颜色 判断发送方向(粗略) 但是每节的第一条颜色是错的. 所以需要以上 name 进行精准判断
         style: {
-            go: 'color:#42B475;padding-left:10px;', //right
-            come: 'color:#006EFE;padding-left:10px;', // left
+            go: "color:#42B475;padding-left:10px;", //right
+            come: "color:#006EFE;padding-left:10px;", // left
         },
     },
 
@@ -23,15 +27,29 @@ module.exports = {
     // 如果都没匹配到 才会用这里的 Name 。
     // Num 是一直会用的
 
-    rightNum: '110',
-    rightName: 'name~',
+    rightNum: "110",
+    rightName: "name~",
 
-    leftNum: '119',
-    leftName: ' name',
+    leftNum: "119",
+    leftName: " name",
 
-    imgWebPublicDir: './data/qq-pc/img',
-    faceWebPublicDir: './data/qq-pc/face',
-    fileWebPublicDir: './data/qq-pc/file',
+    rootPath: "QQ-PC-MHT-12345678-20230101",
+
+    DIST_DIR: path.join(__dirname, "./dist/"),
 };
 
-// 385219308
+config.DIST_DIR_TEMP = path.join(config.DIST_DIR, "_temp");
+
+// 资源目录
+// 应该要修改成 `./data/${config.rootPath}/img` 的形式，
+// 但是 php 脚本也需要修改，要调整 PHP 中 IMG_DIR 为入参形式...
+// 不会改 暂且这样吧
+
+config.imgWebPublicDir = `./data/qq-pc/img`;
+config.faceWebPublicDir = `./data/qq-pc/face`;
+config.fileWebPublicDir = `./data/qq-pc/file`;
+
+fs.mkdirpSync(config.DIST_DIR);
+fs.mkdirpSync(config.DIST_DIR_TEMP);
+
+module.exports = config;
--- index.js
+++ index.js
@@ -2,35 +2,36 @@
  * @Description:
  * @Author: lqzh
  * @Date: 2020-02-28 10:52:24
- * @LastEditTime: 2023-01-31 17:01:35
+ * @LastEditTime: 2023-06-04 22:57:33
  */
-const cheerio = require('cheerio');
-const fs = require('fs');
-const dayjs = require('dayjs');
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
+const dayjs = require("dayjs");
 
-const config = require('./config');
-const type = require('./lib/type');
-const { checkJSON } = require('./lib/check');
-const mhtToHtml = require('./lib/mht');
-const { getAllNames, fixDirectionName } = require('./lib/utils');
-const { CURR_NAME, NONE_NAME_REPLACE_KEY_LEFT, NONE_NAME_REPLACE_KEY_RIGHT } = require('./lib/directionName');
-const { fixImgExt } = require('./lib/fixImgExt');
-
-if (!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
-}
+const config = require("./config");
+const type = require("./lib/type");
+const { checkJSON } = require("./lib/check");
+const mhtToHtml = require("./lib/mht");
+const { getAllNames, fixDirectionName } = require("./lib/utils");
+const {
+    CURR_NAME,
+    NONE_NAME_REPLACE_KEY_LEFT,
+    NONE_NAME_REPLACE_KEY_RIGHT,
+} = require("./lib/directionName");
+const { fixImgExt } = require("./lib/fixImgExt");
 
 (async () => {
     mhtToHtml();
 
-    const html = fs.readFileSync('./dist/text0.html');
-    console.info('reading');
+    const html = fs.readFileSync("./dist/text0.html");
+    console.info("reading");
     const $ = cheerio.load(html, { decodeEntities: false });
-    console.info('loading');
+    console.info("loading");
 
     const dataArr = [];
 
-    const $trs = $('table tr');
+    const $trs = $("table tr");
 
     // 匹配到名字后只需要回写一次之前的
     let firstMatchName = {
@@ -38,29 +39,43 @@ if (!fs.existsSync('./dist')) {
         right: false,
     };
 
-    let day = '';
+    let day = "";
     for (let i = 0; i < $trs.length; i++) {
         // if (i >= 10) break;
         const $tr = $trs.eq(i);
 
         const data = type($tr, i, $trs);
-        if (data[0] === 'day') {
+        if (data[0] === "day") {
             day = data[1].content;
             continue;
         }
         switch (data[0]) {
-            case 'msg':
-                data[1].day = dayjs(day).format('YYYY-MM-DD');
+            case "msg":
+                data[1].day = dayjs(day).format("YYYY-MM-DD");
                 data[1].ms = dayjs(`${data[1].day} ${data[1].time}`).valueOf();
                 dataArr.push(data[1]);
 
                 // 如果从 mht 中匹配到名字了 就把之前消息的名字回填
-                if (!firstMatchName.left && CURR_NAME.LEFT !== NONE_NAME_REPLACE_KEY_LEFT) {
-                    fixDirectionName(dataArr, NONE_NAME_REPLACE_KEY_LEFT, CURR_NAME.LEFT);
+                if (
+                    !firstMatchName.left &&
+                    CURR_NAME.LEFT !== NONE_NAME_REPLACE_KEY_LEFT
+                ) {
+                    fixDirectionName(
+                        dataArr,
+                        NONE_NAME_REPLACE_KEY_LEFT,
+                        CURR_NAME.LEFT
+                    );
                     firstMatchName.left = true;
                 }
-                if (!firstMatchName.right && CURR_NAME.RIGHT !== NONE_NAME_REPLACE_KEY_RIGHT) {
-                    fixDirectionName(dataArr, NONE_NAME_REPLACE_KEY_RIGHT, CURR_NAME.RIGHT);
+                if (
+                    !firstMatchName.right &&
+                    CURR_NAME.RIGHT !== NONE_NAME_REPLACE_KEY_RIGHT
+                ) {
+                    fixDirectionName(
+                        dataArr,
+                        NONE_NAME_REPLACE_KEY_RIGHT,
+                        CURR_NAME.RIGHT
+                    );
                     firstMatchName.right = true;
                 }
 
@@ -72,19 +87,26 @@ if (!fs.existsSync('./dist')) {
 
     // 如果名字都没有匹配到， 例如全部是空  那么使用 config 中的默认名字
     if (CURR_NAME.LEFT === NONE_NAME_REPLACE_KEY_LEFT) {
-        console.warn('❌', '没有匹配到 left name');
+        console.warn("❌", "没有匹配到 left name");
         fixDirectionName(dataArr, NONE_NAME_REPLACE_KEY_LEFT, config.leftName);
     }
     if (CURR_NAME.RIGHT === NONE_NAME_REPLACE_KEY_RIGHT) {
-        console.warn('❌', '没有匹配到 right name');
-        fixDirectionName(dataArr, NONE_NAME_REPLACE_KEY_RIGHT, config.rightName);
+        console.warn("❌", "没有匹配到 right name");
+        fixDirectionName(
+            dataArr,
+            NONE_NAME_REPLACE_KEY_RIGHT,
+            config.rightName
+        );
     }
 
     checkJSON(dataArr);
     getAllNames(dataArr);
-    console.log('dataArr.length', dataArr.length);
+    console.log("dataArr.length", dataArr.length);
     const dataText = await fixImgExt(dataArr);
 
-    fs.writeFileSync(`./dist/msg-qq-pc-mht.json`, dataText);
-    console.info('ok');
+    fs.writeFileSync(
+        path.join(config.DIST_DIR, `${config.rootPath}.json`),
+        dataText
+    );
+    console.info("ok");
 })();
